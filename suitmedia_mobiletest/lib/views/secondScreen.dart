e2350ef171922546af22b0fcdcee0d187c581e72
Muhammad Linggar Ryanidha <m.linggar2412@gmail.com>
2023-07-11 23:55:51 +0700

    finish second screen, create third screen

--- suitmedia_mobiletest/lib/views/secondScreen.dart
+++ suitmedia_mobiletest/lib/views/secondScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:suitmedia_mobiletest/controller/selectedUserController.dart';
+import 'package:suitmedia_mobiletest/views/thirdScreen.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
@@ -17,28 +19,79 @@ class SecondScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 color: Colors.black)),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(left: 32),
-            child: Column(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        child: GetBuilder<SelectedUserController>(
+          init: SelectedUserController(),
+          builder: (controller) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Welcome',
-                    style: GoogleFonts.poppins(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.black)),
-                // const SizedBox(height: 8),
-                Text(data,
-                    style: GoogleFonts.poppins(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black)),
+                Container(
+                  padding: const EdgeInsets.only(left: 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Welcome',
+                          style: GoogleFonts.poppins(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black)),
+                      // const SizedBox(height: 8),
+                      Text(data,
+                          style: GoogleFonts.poppins(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black)),
+                    ],
+                  ),
+                ),
+                Center(
+                    child: Text(
+                  controller.SelectedUser ?? 'Select User Name',
+                  style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black),
+                )),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      var result = await Get.to(() =>  ThirdScreen(),
+                          arguments: data);
+                      if (result != null) {
+                        controller.SetSelectedUser(result[1], result[2]);
+                      }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color(0XFF2B637B)),
+                      minimumSize: const MaterialStatePropertyAll(
+                          Size(double.infinity, 50)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'NEXT',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
               ],
-            ),
-          )
-        ],
+            );
+          },
+        ),
       ),
     );
   }
---
+++ suitmedia_mobiletest/lib/views/thirdScreen.dart
@@ -0,0 +1,66 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
+import 'package:suitmedia_mobiletest/controller/paginationController.dart';
+import 'package:suitmedia_mobiletest/models/repository.dart';
+import 'package:suitmedia_mobiletest/widgets/userList.dart';
+
+class ThirdScreen extends StatelessWidget {
+  ThirdScreen({super.key});
+  final data = Get.arguments;
+  final PaginationController paginationController =
+      Get.put(PaginationController(Repository(Dio())));
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Third Screen',
+            style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
+      ),
+      body: SafeArea(
+        child: RefreshIndicator(
+          onRefresh: () async {
+            await paginationController.refreshUsers();
+          },
+          child: Obx(() {
+            final users = paginationController.users;
+
+            if (users.isEmpty) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Get.back(result: [
+                        data[0],
+                        user.first_name,
+                        user.last_name,
+                        user.email,
+                        user.avatar
+                        ]);
+                    },
+                    child: UserList(
+                        firstName: user.first_name,
+                        lastName: user.last_name,
+                        email: user.email,
+                        avatar: user.avatar
+                    ),
+
+                  );
+                });
+          }),
+        ),
+      ),
+    );
+  }
+}
